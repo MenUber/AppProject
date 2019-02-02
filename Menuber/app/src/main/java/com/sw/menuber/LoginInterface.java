c41d5bfd33fb83bdc2e2395626b32dc3eeceaba1
joelbejar <joel.bejar.mallma@gmail.com>
2019-02-02 13:10:12 -0500

    agregando validacion de campos requeridos

--- Menuber/app/src/main/java/com/sw/menuber/LoginInterface.java
+++ Menuber/app/src/main/java/com/sw/menuber/LoginInterface.java
@@ -4,14 +4,26 @@ public interface LoginInterface {
 
     interface View{
 
+        void setRequiredEmail();
+        void setRequiredPass();
+        void navigateToHome();
+
     }
 
     interface Presenter{
 
+        void validateUserPresenter(String user, String pass);
     }
 
     interface Model{
+        void validateUserModel( String user, String pass,loginListener listener);
+    }
 
+    interface loginListener{
+
+        void setRequiredEmail();
+        void setRequiredPass();
+        void successOperation();
     }
 
 
--- Menuber/app/src/main/java/com/sw/menuber/LoginPresenter.java
+++ Menuber/app/src/main/java/com/sw/menuber/LoginPresenter.java
@@ -1,4 +1,52 @@
 package com.sw.menuber;
 
-public class LoginPresenter {
+public class LoginPresenter implements LoginInterface.Presenter,LoginInterface.loginListener {
+
+    private LoginInterface.View view;
+    private LoginInterface.Model model;
+
+    public LoginPresenter(LoginInterface.View view){
+        this.view=view;
+        model = new LoginModel();
+    }
+    @Override
+    public void validateUserPresenter(String user, String pass) {
+        model.validateUserModel(user,pass,this);
+    }
+
+/*    @Override
+    public void setErrorEmail() {
+        if(view!=null){
+            view.setErrorEmail();
+        }
+    }
+
+    @Override
+    public void setErrorPass() {
+        if(view!=null){
+            view.setErrorPass();
+        }
+    }*/
+
+    @Override
+    public void setRequiredEmail() {
+        if(view!=null){
+            view.setRequiredEmail();
+        }
+    }
+
+    @Override
+    public void setRequiredPass() {
+        if(view!=null){
+            view.setRequiredPass();
+        }
+    }
+
+    @Override
+    public void successOperation() {
+        if(view!=null){
+            view.navigateToHome();
+        }
+    }
+
 }
